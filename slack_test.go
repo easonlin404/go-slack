0b05b58adcfc3caacb6af695fb87fe0d4091ff42
easonlin <easonlin@catchplay.com>
2017-06-03 16:25:22 +0800

    Update tests

--- slack_test.go
+++ slack_test.go
@@ -10,11 +10,27 @@ func TestSendMessage(t *testing.T) {
 	// You need get your slack WebhookURL from https://api.slack.com/incoming-webhooks
 	webhookURL := os.Getenv("webhookURL")
 	message := Message{
+		Text: "message",
+	}
+
+	r, e := New().Debug(true).
+		WebhookURL(webhookURL).
+		SendMessage(message)
+
+	assert.NoError(t, e)
+	assert.Equal(t, 200, r.StatusCode)
+
+}
+
+func TestSendMessageWithAttachments(t *testing.T) {
+	// You need get your slack WebhookURL from https://api.slack.com/incoming-webhooks
+	webhookURL := os.Getenv("webhookURL")
+	message := Message{
 		Attachments: []Attachment{
-			{Title: "Order Status", MrkdwnIn: []string{"text"},
+			{Title: "title", MrkdwnIn: []string{"text"},
 				Fields: []Field{
-					{Title: "Request", Value: 0, Short: true},
-					{Title: "Failed", Value: 0, Short: true},
+					{Title: "T1", Value: 0, Short: true},
+					{Title: "T2", Value: 0, Short: true},
 				},
 			},
 		},
